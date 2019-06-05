499cef5a270082db9eca474b8340172831d6ca4b
Fatezhang <762344938@qq.com>
2019-06-05 16:24:38 +0800

    项目pom updte

---
+++ src/main/java/com/zhang/HiBatis/HiBatisClient.java
@@ -0,0 +1,14 @@
+package com.zhang.HiBatis;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @Author zhangjiaheng
+ * @Description 模拟MyBatis
+ **/
+public class HiBatisClient {
+
+    public static void main(String[] args) {
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+    }
+}
